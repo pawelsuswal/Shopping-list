feee657151cd2e6cdc526c1ebc91f77e5b099cdd
Paweł Suswał <pawel.suswal@gmail.com>
2022-12-20 13:27:01 +0100

    Moved supporting functions to mixin class in seperate file for shopping list, views.py updated accordingly

---
+++ shopping_list/mixins.py
@@ -0,0 +1,161 @@
+from django.core.exceptions import ObjectDoesNotExist
+
+from products.models import Product, UNITS_OF_MEASUREMENT
+from products.views import get_categories_by_favourite
+from shopping_list.models import ShoppingList, ProductShoppingList
+
+
+class ShoppingListMixins:
+    def get_all_products(self, user, shopping_list=None):
+        """Returns list of all products split by favourites and categories for
+         selected user with current values from shopping list if provided"""
+        all_products = Product.objects.filter(user=user).order_by('-is_favourite', 'name')
+        categories_for_favourite_products, categories_for_not_favourite_products = get_categories_by_favourite(
+            all_products)
+
+        products_by_categories_and_favourites = [[True, []], [False, []]]
+
+        for data_set in products_by_categories_and_favourites:
+            is_favourite = data_set[0]
+
+            if is_favourite:
+                category_set = categories_for_favourite_products
+            else:
+                category_set = categories_for_not_favourite_products
+
+            for category in category_set:
+                if category == '':
+                    category = None
+
+                products_for_category = all_products.filter(category__name=category,
+                                                            is_favourite=is_favourite).order_by(
+                    'name')
+
+                products_to_save = []
+
+                for product in products_for_category:
+                    if shopping_list and product in shopping_list.products.all():
+                        product_from_shopping_list = shopping_list.productshoppinglist_set.get(product=product)
+                        product_to_save = {
+                            'id': product.id,
+                            'checked': True,
+                            'name': product.name,
+                            'amount': product_from_shopping_list.amount,
+                            'uom': product_from_shopping_list.unit_of_measurement,
+                            'comment': product_from_shopping_list.comment,
+                        }
+
+                    else:
+                        product_to_save = {
+                            'id': product.id,
+                            'checked': False,
+                            'name': product.name,
+                            'amount': product.default_amount,
+                            'uom': product.default_uom,
+                            'comment': '',
+                        }
+
+                    products_to_save.append(product_to_save)
+
+                if products_to_save:
+                    data_set[1].append([category, products_to_save])
+
+        return products_by_categories_and_favourites
+
+    def get_products_for_shopping_list(self, user, post_data):
+        """Return list of product with parameters provided in post_data"""
+        products_for_shopping_list = []
+
+        for item in post_data.items():
+            if 'product' not in item[0]:
+                continue
+
+            product_id = int(item[0].split('-id-')[1])
+            product = Product.objects.filter(user=user, id=product_id)
+
+            if not product:
+                raise ObjectDoesNotExist
+
+            product = product.first()
+            product_id = str(product_id)
+            amount = post_data[''.join(('amount-id-', product_id))]
+            uom = post_data[''.join(('uom-id-', product_id))]
+            comment = post_data[''.join(('comment-id-', product_id))]
+
+            if amount == '':
+                amount = None
+            else:
+                amount = float(amount)
+
+            if not any(uom in uom_code for uom_code in UNITS_OF_MEASUREMENT) and uom != 'None':
+                raise ValueError
+
+            if uom == 'None' or uom == '':
+                uom = None
+
+            products_for_shopping_list.append({
+                'product': product,
+                'amount': amount,
+                'uom': uom,
+                'comment': comment,
+            })
+        return products_for_shopping_list
+
+    def save_products_in_shopping_list(self, products_to_save, shopping_list):
+        """Saves products for shopping list in database"""
+        for product_to_save in products_to_save:
+            shopping_list.productshoppinglist_set.update_or_create(
+                product=product_to_save['product'],
+                defaults={
+                    'amount': product_to_save['amount'],
+                    'unit_of_measurement': product_to_save['uom'],
+                    'comment': product_to_save['comment'],
+                }
+            )
+
+    def clear_products_in_shopping_list(self, shopping_list: ShoppingList):
+        """Removes all products from shopping list"""
+        for product in shopping_list.productshoppinglist_set.all():
+            product.delete()
+
+    def get_shopping_lists(self, shopping_lists, user=None):
+        """Returns list of shopping lists with products related to each of them ordered by category"""
+        uom = dict(UNITS_OF_MEASUREMENT)
+        data_to_render = []
+        for shopping_list in shopping_lists:
+            products = []
+            if shopping_list.shop is None:
+                products_query_set = shopping_list.productshoppinglist_set.all().order_by('product__name')
+                for product in products_query_set.all():
+                    if product.amount is None:
+                        product.amount = ''
+                    if product.unit_of_measurement is not None:
+                        product.unit_of_measurement = uom[product.unit_of_measurement]
+                    products.append(product)
+
+            else:
+                products = ProductShoppingList.objects.raw(
+                    '''select * from shopping_list_productshoppinglist as psl 
+                    left join products_product as p on p.id = psl.product_id
+                    left join shops_shopcategory as sc on (p.category_id = sc.category_id and sc.shop_id = %s )
+                    where shopping_list_id = %s
+                    ''',
+                    [shopping_list.shop_id, shopping_list.id])
+                products = [item for item in products]
+
+                for product in products:
+                    if product.order is None:
+                        product.order = -1
+                    if product.amount is None:
+                        product.amount = ''
+                    if product.unit_of_measurement is not None:
+                        product.unit_of_measurement = uom[product.unit_of_measurement]
+
+                products = sorted(products, key=lambda x: (x.order, x.product.name,))
+
+            shared = False
+            if user is not None and shopping_list.user != user:
+                shared = True
+
+            data_to_render.append([shopping_list, products, shared])
+        return data_to_render
--- shopping_list/views.py
+++ shopping_list/views.py
@@ -1,5 +1,3 @@
-import itertools
-
 from django import views
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q
@@ -8,16 +6,16 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.utils.text import slugify
-from django.views.generic import CreateView, UpdateView, DeleteView, DetailView
+from django.views.generic import CreateView, UpdateView, DeleteView
 
 from friends.models import UserFriend
-from products.models import Product, UNITS_OF_MEASUREMENT
-from products.views import get_categories_by_favourite
-from shopping_list.forms import CreateShoppingListForm, ShowCommentForm
-from shopping_list.models import ShoppingList, ProductShoppingList
+from products.models import UNITS_OF_MEASUREMENT
+from shopping_list.forms import CreateShoppingListForm
+from shopping_list.mixins import ShoppingListMixins
+from shopping_list.models import ShoppingList
 
 
-class ShoppingListCreateView(LoginRequiredMixin, CreateView):
+class ShoppingListCreateView(LoginRequiredMixin, CreateView, ShoppingListMixins):
     """Display view for create new category"""
     success_url = reverse_lazy('shopping_list:list')
     model = ShoppingList
@@ -51,7 +49,7 @@ class ShoppingListCreateView(LoginRequiredMixin, CreateView):
         else:
             source_shopping_list = None
 
-        context['products_by_categories_and_favourites'] = get_all_products(user, source_shopping_list)
+        context['products_by_categories_and_favourites'] = self.get_all_products(user, source_shopping_list)
         context['UNITS_OF_MEASUREMENT'] = UNITS_OF_MEASUREMENT
         # context['source_shopping_list'] = source_shopping_list
         return context
@@ -61,13 +59,13 @@ class ShoppingListCreateView(LoginRequiredMixin, CreateView):
         user = self.request.user
         form.instance.user = user
         try:
-            products_to_save = get_products_for_shopping_list(user, self.request.POST)
+            products_to_save = self.get_products_for_shopping_list(user, self.request.POST)
             shopping_list = form.save()
             user_id = str(shopping_list.user_id)
             shopping_list_id = str(shopping_list.id)
             shopping_list.slug = slugify('-'.join((user_id, shopping_list_id)))
             shopping_list.save()
-            save_products_in_shopping_list(products_to_save, shopping_list)
+            self.save_products_in_shopping_list(products_to_save, shopping_list)
         except ValueError:
             return super().form_invalid(form)
         except ObjectDoesNotExist:
@@ -84,7 +82,7 @@ class ShoppingListCreateView(LoginRequiredMixin, CreateView):
         return kwargs
 
 
-class ShoppingListUpdateView(LoginRequiredMixin, UpdateView):
+class ShoppingListUpdateView(LoginRequiredMixin, UpdateView, ShoppingListMixins):
     """Display view for editing existing shopping list"""
     success_url = reverse_lazy('shopping_list:list')
     model = ShoppingList
@@ -98,7 +96,7 @@ class ShoppingListUpdateView(LoginRequiredMixin, UpdateView):
 
         shopping_list = context['shoppinglist']
 
-        context['products_by_categories_and_favourites'] = get_all_products(user, shopping_list)
+        context['products_by_categories_and_favourites'] = self.get_all_products(user, shopping_list)
         context['UNITS_OF_MEASUREMENT'] = UNITS_OF_MEASUREMENT
         return context
 
@@ -116,10 +114,10 @@ class ShoppingListUpdateView(LoginRequiredMixin, UpdateView):
         user = self.request.user
         form.instance.user = user
         try:
-            products_to_save = get_products_for_shopping_list(user, self.request.POST)
+            products_to_save = self.get_products_for_shopping_list(user, self.request.POST)
             shopping_list = form.save()
-            clear_products_in_shopping_list(shopping_list)
-            save_products_in_shopping_list(products_to_save, shopping_list)
+            self.clear_products_in_shopping_list(shopping_list)
+            self.save_products_in_shopping_list(products_to_save, shopping_list)
         except ValueError:
             return super().form_invalid(form)
         except ObjectDoesNotExist:
@@ -136,7 +134,7 @@ class ShoppingListDeleteView(LoginRequiredMixin, DeleteView):
     extra_context = {'cancel_operation_url': 'shopping_list:list'}
 
 
-class ShoppingListListView(LoginRequiredMixin, views.View):
+class ShoppingListListView(LoginRequiredMixin, views.View, ShoppingListMixins):
     """Display all active shopping lists for logged user"""
 
     def get(self, request):
@@ -145,7 +143,7 @@ class ShoppingListListView(LoginRequiredMixin, views.View):
 
         shopping_lists = ShoppingList.objects.filter(Q(user=user) | Q(shared_with_list=user),
                                                      is_finished=False).order_by('-created_on')
-        data_to_render = get_shopping_lists(shopping_lists, user)
+        data_to_render = self.get_shopping_lists(shopping_lists, user)
 
         return render(request, 'shopping_list/main_list.html', {'data_to_render': data_to_render})
 
@@ -193,10 +191,8 @@ class ShoppingListViewComment(LoginRequiredMixin, views.View):
 
 class ShoppingListUpdateFinishStatus(LoginRequiredMixin, views.View):
     """Change selected shopping list finished status to opposite"""
-
-    # todo czy to jest poprawnie, że baza jest modyfikowana get'em? Przenieść na POST
     def post(self, request, slug):
-        """Try to get selected shopping list for current user and change it finised status to opposite"""
+        """Try to get selected shopping list for current user and change it finished status to opposite"""
         user = request.user
         shopping_list = ShoppingList.objects.filter(user=user, slug=slug)
 
@@ -208,7 +204,7 @@ class ShoppingListUpdateFinishStatus(LoginRequiredMixin, views.View):
         return redirect('shopping_list:list')
 
 
-class ShoppingListHistoryListView(LoginRequiredMixin, views.View):
+class ShoppingListHistoryListView(LoginRequiredMixin, views.View, ShoppingListMixins):
     """Display all finished shopping lists for logged user"""
 
     def get(self, request):
@@ -216,12 +212,12 @@ class ShoppingListHistoryListView(LoginRequiredMixin, views.View):
         user = request.user
 
         shopping_lists = ShoppingList.objects.filter(user=user, is_finished=True).order_by('-created_on')
-        data_to_render = get_shopping_lists(shopping_lists)
+        data_to_render = self.get_shopping_lists(shopping_lists)
 
         return render(request, 'shopping_list/history_list.html', {'data_to_render': data_to_render})
 
 
-class ShoppingListFavouritesListView(LoginRequiredMixin, views.View):
+class ShoppingListFavouritesListView(LoginRequiredMixin, views.View, ShoppingListMixins):
     """Display all favourite shopping lists for logged user"""
 
     def get(self, request):
@@ -229,7 +225,7 @@ class ShoppingListFavouritesListView(LoginRequiredMixin, views.View):
         user = request.user
 
         shopping_lists = ShoppingList.objects.filter(user=user, is_favourite=True).order_by('created_on')
-        data_to_render = get_shopping_lists(shopping_lists)
+        data_to_render = self.get_shopping_lists(shopping_lists)
 
         return render(request, 'shopping_list/favourites_list.html', {'data_to_render': data_to_render})
 
@@ -290,159 +286,4 @@ class ShoppingListShareView(LoginRequiredMixin, UpdateView):
         return context
 
 
-def get_all_products(user, shopping_list=None):
-    """Returns list of all products split by favourites and categories for
-     selected user with current values from shopping list if provided"""
-    all_products = Product.objects.filter(user=user).order_by('-is_favourite', 'name')
-    categories_for_favourite_products, categories_for_not_favourite_products = get_categories_by_favourite(
-        all_products)
 
-    products_by_categories_and_favourites = [[True, []], [False, []]]
-
-    for data_set in products_by_categories_and_favourites:
-        is_favourite = data_set[0]
-
-        if is_favourite:
-            category_set = categories_for_favourite_products
-        else:
-            category_set = categories_for_not_favourite_products
-
-        for category in category_set:
-            if category == '':
-                category = None
-
-            products_for_category = all_products.filter(category__name=category, is_favourite=is_favourite).order_by(
-                'name')
-
-            products_to_save = []
-
-            for product in products_for_category:
-                if shopping_list and product in shopping_list.products.all():
-                    product_from_shopping_list = shopping_list.productshoppinglist_set.get(product=product)
-                    product_to_save = {
-                        'id': product.id,
-                        'checked': True,
-                        'name': product.name,
-                        'amount': product_from_shopping_list.amount,
-                        'uom': product_from_shopping_list.unit_of_measurement,
-                        'comment': product_from_shopping_list.comment,
-                    }
-
-                else:
-                    product_to_save = {
-                        'id': product.id,
-                        'checked': False,
-                        'name': product.name,
-                        'amount': product.default_amount,
-                        'uom': product.default_uom,
-                        'comment': '',
-                    }
-
-                products_to_save.append(product_to_save)
-
-            if products_to_save:
-                data_set[1].append([category, products_to_save])
-
-    return products_by_categories_and_favourites
-
-
-def get_products_for_shopping_list(user, post_data):
-    """Return list of product with parameters provided in post_data"""
-    products_for_shopping_list = []
-
-    for item in post_data.items():
-        if 'product' not in item[0]:
-            continue
-
-        product_id = int(item[0].split('-id-')[1])
-        product = Product.objects.filter(user=user, id=product_id)
-
-        if not product:
-            raise ObjectDoesNotExist
-
-        product = product.first()
-        product_id = str(product_id)
-        amount = post_data[''.join(('amount-id-', product_id))]
-        uom = post_data[''.join(('uom-id-', product_id))]
-        comment = post_data[''.join(('comment-id-', product_id))]
-
-        if amount == '':
-            amount = None
-        else:
-            amount = float(amount)
-
-        if not any(uom in uom_code for uom_code in UNITS_OF_MEASUREMENT) and uom != 'None':
-            raise ValueError
-
-        if uom == 'None' or uom == '':
-            uom = None
-
-        products_for_shopping_list.append({
-            'product': product,
-            'amount': amount,
-            'uom': uom,
-            'comment': comment,
-        })
-    return products_for_shopping_list
-
-
-def save_products_in_shopping_list(products_to_save, shopping_list):
-    """Saves products for shopping list in database"""
-    for product_to_save in products_to_save:
-        shopping_list.productshoppinglist_set.update_or_create(
-            product=product_to_save['product'],
-            defaults={
-                'amount': product_to_save['amount'],
-                'unit_of_measurement': product_to_save['uom'],
-                'comment': product_to_save['comment'],
-            }
-        )
-
-
-def clear_products_in_shopping_list(shopping_list: ShoppingList):
-    """Removes all products from shopping list"""
-    for product in shopping_list.productshoppinglist_set.all():
-        product.delete()
-
-
-def get_shopping_lists(shopping_lists, user=None):
-    """Returns list of shopping lists with products related to each of them ordered by category"""
-    uom = dict(UNITS_OF_MEASUREMENT)
-    data_to_render = []
-    for shopping_list in shopping_lists:
-        products = []
-        if shopping_list.shop is None:
-            products_query_set = shopping_list.productshoppinglist_set.all().order_by('product__name')
-            for product in products_query_set.all():
-                if product.amount is None:
-                    product.amount = ''
-                if product.unit_of_measurement is not None:
-                    product.unit_of_measurement = uom[product.unit_of_measurement]
-                products.append(product)
-
-        else:
-            products = ProductShoppingList.objects.raw(
-                '''select * from shopping_list_productshoppinglist as psl 
-                left join products_product as p on p.id = psl.product_id
-                left join shops_shopcategory as sc on (p.category_id = sc.category_id and sc.shop_id = %s )
-                where shopping_list_id = %s
-                ''',
-                [shopping_list.shop_id, shopping_list.id])
-            products = [item for item in products]
-
-            for product in products:
-                if product.order is None:
-                    product.order = -1
-                if product.amount is None:
-                    product.amount = ''
-                if product.unit_of_measurement is not None:
-                    product.unit_of_measurement = uom[product.unit_of_measurement]
-
-            products = sorted(products, key=lambda x: (x.order, x.product.name,))
-
-        shared = False
-        if user is not None and shopping_list.user != user:
-            shared = True
-
-        data_to_render.append([shopping_list, products, shared])
-    return data_to_render
