913bc1a6a59c6b71693cdefc4ba52c7f76908278
Paweł Suswał <pawel.suswal@gmail.com>
2022-12-20 14:17:34 +0100

    Added lacking models to admin panel

--- friends/admin.py
+++ friends/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from friends import models
+
 # Register your models here.
+admin.site.register(models.Invite)
+admin.site.register(models.UserFriend)
--- shopping_list/admin.py
+++ shopping_list/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from shopping_list import models
+
 # Register your models here.
+admin.site.register(models.ShoppingList)
+admin.site.register(models.ProductShoppingList)
